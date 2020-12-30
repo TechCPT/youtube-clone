c424bc16020dc9b683c52c7dce1f97c900175fd7
TechCPT <paktungchow04@gmail.com>
2020-12-30 12:33:37 -0500

    Created header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello TechCPT, let's build a YouTube Clone!</h1>
+      {/* <h1>Hello TechCPT, let's build a YouTube Clone!</h1> */}
+
+    <Header />
+
+      {/* Header -> <Header /> */}
+      {/* Sidebar -> <Sidebar /> */}
+      {/* RecommendedVideos */}
+
     </div>
   );
 }
